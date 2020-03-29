1e69ddc5a26b4ccc1f2e005f1902be3247b2cb91
Kotaro Abe <abekoh.bcky@gmail.com>
2020-03-30 00:42:14 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eu
 APP_YAML=app.yaml
 mv ${APP_YAML} ${APP_YAML}.bak
-cat ${APP_YAML}.bak | sed -e "s/{{ google.api.api-key }}/${GOOGLE_API_API_KEY}/g" > ${APP_YAML}
+cat ${APP_YAML}.bak | sed -e "s/{{ google.api.api-key }}/${GOOGLE_API_API_KEY}/g" \
+  | sed -e"s/{{ BASIC_USERNAME }}/${BASIC_USERNAME}/g" \
+  | sed -e"s/{{ BASIC_PASSWORD }}/${BASIC_PASSWORD}/g" > ${APP_YAML}
 gcloud app deploy
 mv ${APP_YAML}.bak ${APP_YAML}
